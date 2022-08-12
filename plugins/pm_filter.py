2dabaf890e109852c434dd65d5f97e6147630bf9
Ansh vachhani <92299607+Anshvachhani99@users.noreply.github.com>
2022-08-12 16:24:53 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -175,7 +175,7 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"♨️[{get_size(file.file_size)}]♨️{file.file_name}", url=f"https://telegram.dog/Testv105robot?start={file.file_id}'
+                    text=f"♨️[{get_size(file.file_size)}]♨️{file.file_name}", url=f"https://telegram.dog/Testv105robot?start={file.file_id}"
                 ),
             ]
             for file in files
@@ -184,7 +184,7 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"♨️[{get_size(file.file_size)}]♨️{file.file_name}", url=f"https://telegram.dog/Testv105robot?start={file.file_id}'
+                    text=f"♨️[{get_size(file.file_size)}]♨️{file.file_name}", url=f"https://telegram.dog/Testv105robot?start={file.file_id}"
                 ),
                 InlineKeyboardButton(
                     text=f"♨️[{get_size(file.file_size)}]♨️",
@@ -1639,7 +1639,7 @@ async def auto_filter(client, msg, spoll=False):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"[{get_size(file.file_size)}] {file.file_name}", url=f"https://telegram.dog/Testv105robot?start={file.file_id}'
+                    text=f"[{get_size(file.file_size)}] {file.file_name}", url=f"https://telegram.dog/Testv105robot?start={file.file_id}"
                 ),
             ]
             for file in files
